3f1ee1fc8f44ea21672f2a5b105e1244adade126
Devagouda Patil <depatil@devagoudas-mbp.corp.adobe.com>
2019-04-12 13:51:55 -0500

    up

--- kill-selenium.sh
+++ kill-selenium.sh
@@ -3,9 +3,18 @@ how_many () {
 }
 how_many
 while [ $RUNNING_CONTAINERS -gt 1 ]; do
-  echo "how many $RUNNING_CONTAINERS"
+  echo -e "RUNNING TESTS\ncontainers running: $RUNNING_CONTAINERS"
   sleep 10;
   how_many
 done
 echo "tests have finished! stopping selenium..."
-exit
+
+# determine if there were failures
+FAIL_FILE="./dev/tests/acceptance/tests/_output/failed"
+if [ -f $FAIL_FILE ]; then
+    echo "there were failed tests!"
+    cat $FAIL_FILE
+    exit 1
+fi
+
+exit 0
